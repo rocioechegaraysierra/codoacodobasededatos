062ccca5769f9bf74deb856a05e8e5f07fa38783
rocioechegaraysierra <rocio.echegaray.sierra@gmail.com>
2022-10-28 21:58:56 -0300

    Se agregan queries utilizadas

---
+++ queries.sql
@@ -0,0 +1,9 @@
+USE codoacodo;
+
+INSERT INTO cliente VALUES ("1","Matias","Gomez","20",NOW(),"Tucumán");
+INSERT INTO cliente VALUES ("2", "Lucas","Martinez","36",NOW(),"Corrientes");
+INSERT INTO cliente VALUES ("3", "Marcos","Alvarez","40",NOW(),"La Pampa");
+INSERT INTO cliente VALUES ("19","Federico","Gomez","60",NOW(),"Tucumán");
+INSERT INTO cliente VALUES ("15","Alan","Del Valle","25",NOW(),"Buenos Aires");
+
+SELECT * FROM cliente;
